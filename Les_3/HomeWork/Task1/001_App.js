0c3564170bb7485b9c17d7404b153d5547f66c47
Babych <babych.p.v@gmail.com>
2020-11-03 23:25:07 +0200

    HW step 1

--- Les_3/HomeWork/Task1/001_App.js
+++ Les_3/HomeWork/Task1/001_App.js
@@ -1,19 +1,8 @@
 window.onload = function () {
-    let div1 = document.getElementById("div_1");
-
-    for (let i = 0; i < 5; i++) {
-        let p1 = document.createElement("p")
-        p1.innerHTML = "Paragraph " + i;
-        div1.appendChild(p1);
-    }
-
-    setTimeout(function () {
-        let paragraphs = document.getElementsByTagName("p");
-
-        for (let i = 0; i < paragraphs.length; i++) {
-            paragraphs[i].style.textTransform = 'uppercase'
-        }
-    }, 5000)
-
-
+    window.setTimeout(showMessage, 5000);
 }
+
+function showMessage() {
+    window.open("002_ClassWork.htm", "Form", "width=616,height=417,status=yes,resizable=no");
+}
+
