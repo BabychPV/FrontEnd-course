1f909cf5c9b3499a95a308c1b0c327c91e31173c
Babych <babych.p.v@gmail.com>
2020-11-03 22:22:17 +0200

    4

--- Les_3/CW/001_App.js
+++ Les_3/CW/001_App.js
@@ -1,27 +1,8 @@
-function SelectP() {
-
-    let paragraphs = document.getElementsByTagName("p");
-
-    for (let i = 0; i < paragraphs.length; i++) {
-        paragraphs[i].style.border = "4px double red";
-    }
-}
-function SelectD() {
-
-    let div = document.getElementsByTagName("div");
-
-    for (let i = 0; i < div.length; i++) {
-        div[i].style.border = "4px double green";
-    }
-}
-function SelectA() {
-
-    let a = document.getElementsByTagName("a");
-
-    for (let i = 0; i < a.length; i++) {
-        a[i].style.border = "4px double yellow";
-    }
+window.onload = function () {
+    window.setTimeout(showMessage, 5000);
 }
 
-
+function showMessage() {
+    window.open("002_ClassWork.htm", "Form", "width=800,height=800,status=yes,resizable=no");
+}
 
