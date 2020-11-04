e29b87e128488911c7a6d1ac0b3817cf7538275c
Babych <babych.p.v@gmail.com>
2020-11-04 09:58:44 +0200

    HW 4 step 1

---
+++ Les_3/HomeWork/Task4/001_App.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+
+    let data = prompt('Set data xx +/- xx', '10+8')
+
+    parsePromtOutput(data);
+
+    function parsePromtOutput(str) {
+        const sing = ['+', '-', '*', '/'];
+        let cursing;
+        let pos;
+
+        for (let i = 0; i < sing.length; i++) {
+            pos = str.indexOf(sing[i]);
+            if (-1 !== pos) {
+                cursing = sing[i];
+                break;
+            }
+            cursing = 'NaN';
+            pos = NaN;
+        }
+
+        if (cursing === 'NaN') {
+            alert("Error")
+        } else {
+            CalcRun(+str.substr(0, pos).trim(), +str.substr(pos + 1).trim(), cursing)
+        }
+    }
+
+};
