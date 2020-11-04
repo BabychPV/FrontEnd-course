4319996fac88aced156e60f82178e7aaa982ab02
Babych <babych.p.v@gmail.com>
2020-11-04 02:49:11 +0200

    HW step 6

--- Les_3/HomeWork/Task3/001_App.js
+++ Les_3/HomeWork/Task3/001_App.js
@@ -1,30 +1,28 @@
-
 window.onload = function () {
 
-    let data = prompt('Set data xx +/- xx','10+8')
+    let data = prompt('Set data xx +/- xx', '10+8')
 
     parsePromtOutput(data);
 
     function parsePromtOutput(str) {
-        const sing = ['+','-','*','/'];
+        const sing = ['+', '-', '*', '/'];
         let cursing;
         let pos;
 
         for (let i = 0; i < sing.length; i++) {
             pos = str.indexOf(sing[i]);
-            if ( -1 !== pos){
+            if (-1 !== pos) {
                 cursing = sing[i];
                 break;
             }
-            cursing = NaN;
+            cursing = 'NaN';
             pos = NaN;
         }
 
-        if(cursing === NaN){
+        if (cursing === 'NaN') {
             alert("Error")
-        }else
-        {
-            CalcRun(+str.substr(0,pos).trim(),+str.substr(pos+1).trim(),cursing)
+        } else {
+            CalcRun(+str.substr(0, pos).trim(), +str.substr(pos + 1).trim(), cursing)
         }
     }
 
