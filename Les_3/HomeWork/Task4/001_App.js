31f7f28ebc51fbebaded803c7a1e98c428c3f5c9
Babych <babych.p.v@gmail.com>
2020-11-04 16:35:14 +0200

    HW 4 step 2

--- Les_3/HomeWork/Task4/001_App.js
+++ Les_3/HomeWork/Task4/001_App.js
@@ -8,41 +8,60 @@ window.onload = function () {
     let w2 = {};
     let w3 = {};
 
+    function EventCloseChildwin_1() {
+        document.getElementById('p1').innerHTML = 'Урок_№1';
+    }
+
+    function EventCloseChildwin_2() {
+        document.getElementById('p2').innerHTML = 'Урок_№2';
+    }
+
+    function EventCloseChildwin_3() {
+        document.getElementById('p3').innerHTML = 'Урок_№3';
+    }
+
     function openLes_1() {
 
         if (w1.hasOwnProperty('closed') === true) {
             if (w1.closed === false) {
                 w1.close();
-                document.getElementById('p1').innerHTML = 'Урок_1';
                 return;
             }
         }
-        document.getElementById('p1').innerHTML = 'Урок_1 (відкрито)';
-        w1 = window.open("Resource/001_konstruktory i prototipy.pdf", "Урок_1", "width=800,height=800,status=yes,resizable=no,left=500");
+        document.getElementById('p1').innerHTML = 'Урок_№1 (відкрито)';
+        w1 = window.open("Resource/001_konstruktory i prototipy.pdf", "Урок_№1", "width=800,height=800,status=yes,resizable=no,left=500");
+        w1.onload = function () {
+            w1.onunload = EventCloseChildwin_1;
+        }
     }
+
 
     function openLes_2() {
         if (w2.hasOwnProperty('closed') === true) {
             if (w2.closed === false) {
                 w2.close();
-                document.getElementById('p2').innerHTML = 'Урок_2';
                 return;
             }
         }
-        document.getElementById('p2').innerHTML = 'Урок_2 (відкрито)';
-        w2 = window.open("Resource/002_Rabota s dokumentami.pdf", "Урок_2", "width=800,height=800,status=yes,resizable=no,left=500");
+        document.getElementById('p2').innerHTML = 'Урок_№2 (відкрито)';
+        w2 = window.open("Resource/002_Rabota s dokumentami.pdf", "Урок_№2", "width=800,height=800,status=yes,resizable=no,left=500");
+        w2.onload = function () {
+            w2.onunload = EventCloseChildwin_2;
+        }
     }
 
     function openLes_3() {
         if (w3.hasOwnProperty('closed') === true) {
             if (w3.closed === false) {
                 w3.close();
-                document.getElementById('p3').innerHTML = 'Урок_3';
                 return;
             }
         }
-        document.getElementById('p3').innerHTML = 'Урок_3 (відкрито)';
-        w3 = window.open("Resource/003_Window. Regulyarnye vyrazheniya.pdf", "Урок_3", "width=800,height=800,status=yes,resizable=no,left=500");
+        document.getElementById('p3').innerHTML = 'Урок_№3 (відкрито)';
+        w3 = window.open("Resource/003_Window. Regulyarnye vyrazheniya.pdf", "Урок_№3", "width=800,height=800,status=yes,resizable=no,left=500");
+        w3.onload = function () {
+            w3.onunload = EventCloseChildwin_3;
+        }
     }
 
 
