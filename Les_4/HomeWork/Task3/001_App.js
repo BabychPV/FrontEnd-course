2297c2c1891076295c029bb344f9e0cc1fa77963
Babych <babych.p.v@gmail.com>
2020-11-06 14:30:39 +0200

    HW3 end

--- Les_4/HomeWork/Task3/001_App.js
+++ Les_4/HomeWork/Task3/001_App.js
@@ -1,29 +1,40 @@
 window.onload = function () {
-
-    let data = prompt('Set data xx +/- xx', '10+8')
-
-    parsePromtOutput(data);
-
-    function parsePromtOutput(str) {
-        const sing = ['+', '-', '*', '/'];
-        let cursing;
-        let pos;
-
-        for (let i = 0; i < sing.length; i++) {
-            pos = str.indexOf(sing[i]);
-            if (-1 !== pos) {
-                cursing = sing[i];
-                break;
-            }
-            cursing = 'NaN';
-            pos = NaN;
-        }
-
-        if (cursing === 'NaN') {
-            alert("Error")
-        } else {
-            CalcRun(+str.substr(0, pos).trim(), +str.substr(pos + 1).trim(), cursing)
-        }
+    alert(screen.width + "  " + screen.height)
+    const arr = [];
+    for (let i = 0; i < 3000; i++) {
+        arr[i] = document.createElement("div");
+        arr[i].style.backgroundColor = getRandomColor();
+        arr[i].style.height = '100px';
+        arr[i].style.width = '100px';
+        arr[i].style.position = 'absolute';
+        document.body.appendChild(arr[i]);
     }
 
-};
+    setInterval(function () {
+        for (let i = 0; i < arr.length; i++) {
+            let l = randomInRange(0, screen.width - 100);
+            let t = randomInRange(0, screen.height - 100);
+            arr[i].style.left = l + 'px';
+            arr[i].style.top = t + 'px';
+            arr[i].style.height = randomInRange(10,50)+'px';
+            arr[i].style.width = randomInRange(10,60)+'px';
+            // arr[i].textContent = 'left '+l +' top ' + t;
+            // arr[i].style.textAlign = 'center';
+
+        }
+    }, 100)
+}
+
+
+function randomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
