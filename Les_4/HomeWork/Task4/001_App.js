26c9bf8726d56f84612f6e6f33edc1b4faef84a8
Babych <babych.p.v@gmail.com>
2020-11-06 13:56:30 +0200

    HW3 start

--- Les_4/HomeWork/Task4/001_App.js
+++ Les_4/HomeWork/Task4/001_App.js
@@ -1,68 +1,35 @@
 window.onload = function () {
-
-    document.getElementById('p1').addEventListener("click", openLes_1);
-    document.getElementById('p2').addEventListener("click", openLes_2);
-    document.getElementById('p3').addEventListener("click", openLes_3);
-
-    let w1 = {};
-    let w2 = {};
-    let w3 = {};
-
-    function EventCloseChildwin_1() {
-        document.getElementById('p1').innerHTML = 'Урок_№1';
-    }
-
-    function EventCloseChildwin_2() {
-        document.getElementById('p2').innerHTML = 'Урок_№2';
-    }
-
-    function EventCloseChildwin_3() {
-        document.getElementById('p3').innerHTML = 'Урок_№3';
-    }
-
-    function openLes_1() {
-
-        if (w1.hasOwnProperty('closed') === true) {
-            if (w1.closed === false) {
-                w1.close();
-                return;
+    let selector = 0;
+    document.getElementsByTagName('button')[0].onclick = function () {
+        let User = document.getElementsByName('User')[0];
+        let Login = document.getElementsByName('Login')[0];
+        let divPopUp = document.getElementById('divPopUp');
+        if ((User.value === 'Username or Email' || User.value === 'Username or Email') || (Login.value === '')) {
+            setAttribObject(User, '#efabab', Login, '#efabab', divPopUp, 'Ви не заповнили поля логіну та паролю', '#efabab', 'block');
+        } else {
+            if ((User.value === 'admin') && (Login.value === '12345')) {
+                setAttribObject(User, 'white', Login, 'white', divPopUp, 'Вхід виконано', 'green', 'block');
+            } else {
+                setAttribObject(User, '#efabab', Login, '#efabab', divPopUp, 'Логін чи пароль не вірний', '#efabab', 'block');
             }
         }
-        document.getElementById('p1').innerHTML = 'Урок_№1 (відкрито)';
-        w1 = window.open("Resource/001_konstruktory i prototipy.pdf", "Урок_№1", "width=800,height=800,status=yes,resizable=no,left=500");
-        w1.onload = function () {
-            w1.onunload = EventCloseChildwin_1;
-        }
+
+
     }
 
-
-    function openLes_2() {
-        if (w2.hasOwnProperty('closed') === true) {
-            if (w2.closed === false) {
-                w2.close();
-                return;
-            }
-        }
-        document.getElementById('p2').innerHTML = 'Урок_№2 (відкрито)';
-        w2 = window.open("Resource/002_Rabota s dokumentami.pdf", "Урок_№2", "width=800,height=800,status=yes,resizable=no,left=500");
-        w2.onload = function () {
-            w2.onunload = EventCloseChildwin_2;
-        }
-    }
-
-    function openLes_3() {
-        if (w3.hasOwnProperty('closed') === true) {
-            if (w3.closed === false) {
-                w3.close();
-                return;
-            }
-        }
-        document.getElementById('p3').innerHTML = 'Урок_№3 (відкрито)';
-        w3 = window.open("Resource/003_Window. Regulyarnye vyrazheniya.pdf", "Урок_№3", "width=800,height=800,status=yes,resizable=no,left=500");
-        w3.onload = function () {
-            w3.onunload = EventCloseChildwin_3;
-        }
-    }
+}
 
 
-};
+function setAttribObject(in1, in1Color, in2, in2Color, popUp, popUpMess, popUpColor, popUpDisplay) {
+    in1.style.backgroundColor = in1Color;
+    in2.style.backgroundColor = in2Color;
+    popUp.textContent = popUpMess;
+    popUp.style.color = popUpColor;
+    popUp.style.fontSize = '20px';
+    popUp.style.display = popUpDisplay;
+    setTimeout(function () {
+        in1.style.backgroundColor = in1Color;
+        in2.style.backgroundColor = in2Color;
+        popUp.style.display = 'none'
+    }, 2000)
+}
