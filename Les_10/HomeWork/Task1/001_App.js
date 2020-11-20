3aeecbe6d7358f0570a39a40fe5bba1b9f0f2f48
BabychPV <babych.p.v@gmail.com>
2020-11-20 02:44:29 +0200

    Les 10 HW end

--- Les_10/HomeWork/Task1/001_App.js
+++ Les_10/HomeWork/Task1/001_App.js
@@ -1,22 +1,67 @@
-if (window.addEventListener) window.addEventListener("load", init, false);
+﻿if (window.addEventListener) window.addEventListener("load", init, false);
 
 function init() {
 
 
+    async function loadText(url, a, b) {
 
+        let options = {
+            method: 'POST',
+            body: `a=${a}&b=${b}`,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        }
+        const response = await fetch(url, options);
+        if (response.status == 200) {
+            return response.text();
+        } else {
+            alert('Error )');
+        }
+    }
 
-    let img = $('img');
-    let but = $('but');
-    let counter = 1;
+    async function Run() {
 
-    but.addEventListener('click', function(e){
+        let a = $('text1').value;
+        let b = $('text2').value;
+        let data;
 
-        ++counter > 31 ? counter=1:counter;
-        img.src = 'Resource/Tilda_Icons_27_Bullets/Tilda_Icons_27bu_'+ counter + '.svg'
+        try {
+            data = await loadText('CalcHandler.ashx', +a, +b);
+            $('text3').value = data;
+        } catch (err) {
+            if (err.response.status == 404) {
+                alert("Косяк");
+            } else {
+                throw err;
+            }
+        }
+    }
 
+    $('but').addEventListener('click', Run);
+
+    let pattern = /\d/;
+
+    $('text1').addEventListener('keypress', function (e) {
+        var str = String.fromCharCode(e.charCode);
+        if (!pattern.test(str)) {
+            e.preventDefault();
+        }
+    })
+
+    $('text2').addEventListener('keypress', function (e) {
+        var str = String.fromCharCode(e.charCode);
+        if (!pattern.test(str)) {
+            e.preventDefault();
+        }
     })
 
 }
+
+
+
+
+
 
 
 
