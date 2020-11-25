3d087234b0cc18ff261de651c89d9fbbeb739e55
BabychPV <babych.p.v@gmail.com>
2020-11-25 21:08:39 +0200

    Les 3 СW end (Bootstrap)

--- Bootstrap/Les_3/CW/js/main.js
+++ Bootstrap/Les_3/CW/js/main.js
@@ -1,4 +1,15 @@
-let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
-let tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+(function() {
+    'use strict'
+    let forms = document.querySelectorAll('.myForm')
+    Array.prototype.slice.call(forms)
+        .forEach(function(form) {
+            form.addEventListener('submit', function(event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
