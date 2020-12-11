b3e0c6055eccc3dab534a8b92dd13965495599a5
BabychPV <babych.p.v@gmail.com>
2020-12-11 15:12:38 +0200

    Les 4  CW end (HTML&CSS ADV)

--- HTML_Adv/Les4/CW/001_App.js
+++ HTML_Adv/Les4/CW/001_App.js
@@ -1,27 +1,35 @@
-function SelectP() {
+window.addEventListener("load", Init);
 
-    let paragraphs = document.getElementsByTagName("p");
+function Init() {
 
-    for (let i = 0; i < paragraphs.length; i++) {
-        paragraphs[i].style.border = "4px double red";
+    let stepH;
+    let stepV;
+
+    // получение элемента и его графического контекста.
+    let canvas = document.getElementById("canvas");
+    let context = canvas.getContext("2d");
+
+    context.beginPath();
+
+    for (let i = 0; i < 2; i++) {
+        let long;
+        long = i === 0 ? context.canvas.height : context.canvas.width;
+        stepH = 0;
+        stepV = 0;
+        for (let j = 0; j < 40; j++) {
+
+            if (i === 0) {
+                context.lineTo(stepH, long);
+                stepH += 10;
+                stepV = 0;
+            } else {
+                context.lineTo(long, stepV);
+                stepV += 10;
+                stepH = 0;
+            }
+            context.moveTo(stepH, stepV);
+        }
     }
+
+    context.stroke();
 }
-function SelectD() {
-
-    let div = document.getElementsByTagName("div");
-
-    for (let i = 0; i < div.length; i++) {
-        div[i].style.border = "4px double green";
-    }
-}
-function SelectA() {
-
-    let a = document.getElementsByTagName("a");
-
-    for (let i = 0; i < a.length; i++) {
-        a[i].style.border = "4px double yellow";
-    }
-}
-
-
-
