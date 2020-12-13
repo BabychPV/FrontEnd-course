6ec065ef153245fd812e945b1b1745605398f429
Babych <babych.p.v@gmail.com>
2020-12-13 20:43:28 +0200

    Les 5  HW_2 end (HTML&CSS ADV)

---
+++ HTML_Adv/Les5/HW/Task1/001_App.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+
+    let but;
+
+    but = document.getElementById("but1");
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    but.addEventListener('mouseover', function (e) {
+        dropdowns[0].classList.add('view');
+
+    })
+
+    window.addEventListener('mousemove', function (e) {
+       if (this.className != 'dropdown-content'){
+        dropdowns[0].classList.toggle('view');}
+
+    })
+
+
+};
+
